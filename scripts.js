e331c6f4839af29f63a0da31210280628c3a9135
Anastasia <anastasia-aniwa@live.com>
2018-12-04 18:21:03 +0000

    play around

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 alert("This is a test message");
+
+
+$(document).ready(function(){
+    $("btn-twitter").mouseenter(function(){
+        $("btn-twitter").css("background-color", "yellow");
+    });
+    $("btn-twitter").mouseleave(function(){
+        $("btn-twitter").css("background-color", "lightgray");
+    });
+});
+$(document).ready(function(){
+    $("btn-facebook").mouseenter(function(){
+        $("btn-facebook").css("background-color", "yellow");
+    });
+    $("btn-facebook").mouseleave(function(){
+        $("btn-facebook").css("background-color", "lightgray");
+    });
+});
+$(document).ready(function(){
+    $("btn-pinterest").mouseenter(function(){
+        $("btn-pinterest").css("background-color", "yellow");
+    });
+    $("btn-pinterest").mouseleave(function(){
+        $("btn-pinterest").css("background-color", "lightgray");
+    });
+});
