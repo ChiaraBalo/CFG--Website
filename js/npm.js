374e8530a26331ee8e1eeaa80221e7bc4b329695
Anastasia <anastasia-aniwa@live.com>
2018-12-11 00:00:56 +0000

    Inserted Map w/ markers

--- js/npm.js
+++ js/npm.js
@@ -11,3 +11,12 @@ require('../../js/popover.js')
 require('../../js/scrollspy.js')
 require('../../js/tab.js')
 require('../../js/affix.js')
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
